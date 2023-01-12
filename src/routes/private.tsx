8ad47c96cf56881e3b9703011ba4d0708c83433b
vt415204 <ramonuchoa386@gmail.com>
2023-01-12 15:21:58 -0300

    feat: update app routes

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
-
-// organisms
 import Sidebar from './../components/organisms/sidebar';
-
-// Pages
 import Dashboard from './../pages/private/dashboard';
-import ApplicationListPage from './../pages/private/applicationsListPage';
-
-// css
+import ReportListPage from '../pages/private/reportListPage';
 import * as S from './styles';
 
 const Layout = () => (
@@ -23,7 +17,7 @@ const Private = () => {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Dashboard />} />
-        <Route path='apis' element={<ApplicationListPage />} />
+        <Route path='relatorio-de-envio' element={<ReportListPage />} />
       </Route>
     </Routes>
   );
