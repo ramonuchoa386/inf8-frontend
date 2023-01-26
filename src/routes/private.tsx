c1e57f20ad5a567246206e612b733797f54021e6
vt415204 <ramonuchoa386@gmail.com>
2023-01-26 14:58:06 -0300

    feat: remove dashboard from routes

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
 import Sidebar from './../components/organisms/sidebar';
-import Dashboard from './../pages/private/dashboard';
 import ReportListPage from '../pages/private/reportListPage';
 import * as S from './styles';
 
@@ -16,8 +15,7 @@ const Private = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path='relatorio-de-envio' element={<ReportListPage />} />
+        <Route index element={<ReportListPage />} />
       </Route>
     </Routes>
   );
