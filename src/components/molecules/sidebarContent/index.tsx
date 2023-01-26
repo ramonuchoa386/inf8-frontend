8f3e79931311772cc950de5ed149802e20d1b92d
vt415204 <ramonuchoa386@gmail.com>
2023-01-26 14:57:51 -0300

    feat: remove dashboard link from sidebar

--- src/components/molecules/sidebarContent/index.tsx
+++ src/components/molecules/sidebarContent/index.tsx
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react';
 import { Menu, MenuItem } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
-import { BiHome, BiCloudUpload } from 'react-icons/bi';
+import { BiCloudUpload } from 'react-icons/bi';
 import * as S from './styles';
-import theme from '../../../styles/theme';
 import validateUserPermissions from '../../../utils/permissions';
 import { Permissions } from '../../../utils/permissions';
 import AuthContext from '../../../context/auth';
@@ -15,16 +14,11 @@ const SidebarContent = ({ iconSize = 17 }: { iconSize?: number }) => {
     <S.Main>
       <Menu iconShape='square'>
         <Menu>
-          <MenuItem icon={<BiHome size={iconSize} />}>
-            Home
-            <Link to='/' title='Dashboard' color={theme.colors.white} />
-          </MenuItem>
-          <S.Division />
           {validateUserPermissions(state.profile, Permissions['VIEW']) && (
             <>
               <MenuItem icon={<BiCloudUpload size={iconSize} />}>
                 Relatório de envio
-                <Link to='/relatorio-de-envio' title='Relatório de envio' />
+                <Link to='/' title='Relatório de envio' />
               </MenuItem>
               <S.Division />
             </>
