d505474fc266b4875f7e92ea5b418af3e961363d
vt415204 <ramonuchoa386@gmail.com>
2023-01-28 17:16:58 -0300

    feat: udpate fake login page

--- src/pages/public/login/index.tsx
+++ src/pages/public/login/index.tsx
@@ -1,13 +1,49 @@
-import React, { useContext } from 'react';
-import AuthContext from '../../../context/auth';
+import React, { useContext, useState } from 'react';
+import AuthContext, { IUserInfo } from '../../../context/auth';
+import {
+  MOCK_OP_VTAL,
+  MOCK_VW_VTAL,
+  MOCK_CP_MASTER,
+  MOCK_CP_ADMIN,
+} from '../../../context/auth/auth.mock';
+import { Profiles } from '../../../utils/enums';
 
 const Login = () => {
-  const { signIn, state } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
+  const [mockProfile, setMockProfile] =
+    useState<Omit<IUserInfo, 'logged'>>(MOCK_OP_VTAL);
+
+  const handleSelectProfile = (profile: Profiles) => {
+    switch (profile) {
+      case Profiles.VW_VTAL:
+        setMockProfile(() => MOCK_VW_VTAL);
+        break;
+
+      case Profiles.CP_MASTER:
+        setMockProfile(() => MOCK_CP_MASTER);
+        break;
+
+      case Profiles.CP_ADMIN:
+        setMockProfile(() => MOCK_CP_ADMIN);
+        break;
+
+      default:
+        setMockProfile(() => MOCK_OP_VTAL);
+        break;
+    }
+  };
 
   return (
     <>
-      <h1>React Router - Está Logado? {JSON.stringify(state.logged)}</h1>
-      <button onClick={signIn}>Login</button>
+      <h1>Perfil</h1>
+      <select onChange={(e) => handleSelectProfile(e.target.value as Profiles)}>
+        <option value='OP_VTAL'>OP_VTAL</option>
+        <option value='VW_VTAL'>VW_VTAL</option>
+        <option value='CP_MASTER'>CP_MASTER</option>
+        <option value='CP_ADMIN'>CP_ADMIN</option>
+      </select>
+
+      <button onClick={() => signIn(mockProfile)}>Login</button>
     </>
   );
 };
