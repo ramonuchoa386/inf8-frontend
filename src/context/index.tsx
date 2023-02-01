484d57b6a67f2d455109e6bfde2d28d0c1449455
vt415204 <ramonuchoa386@gmail.com>
2023-02-01 14:16:22 -0300

    feat: remove unused context and set authContext on top of all

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 
 import { AuthContextProvider } from './auth';
-import { FavViewContextProvider } from './listViewPreference';
 import { SidebarContextProvider } from './sidebar/context';
 import { ToasterContextProvider } from './toaster';
 import { PopUpContextProvider } from './popup';
-import { LastPageContextProvider } from './lastPage';
 import { ModalContextProvider } from './modal';
 
 const GlobalContext: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <ModalContextProvider>
-      <LastPageContextProvider>
+    <AuthContextProvider>
+      <ModalContextProvider>
         <PopUpContextProvider>
           <ToasterContextProvider>
-            <AuthContextProvider>
-              <SidebarContextProvider>
-                <FavViewContextProvider>{children}</FavViewContextProvider>
-              </SidebarContextProvider>
-            </AuthContextProvider>
+            <SidebarContextProvider>{children}</SidebarContextProvider>
           </ToasterContextProvider>
         </PopUpContextProvider>
-      </LastPageContextProvider>
-    </ModalContextProvider>
+      </ModalContextProvider>
+    </AuthContextProvider>
   );
 };
 
