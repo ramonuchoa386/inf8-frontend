78c8e658f23ef1374bd12ee4e99b562408e74816
vt415204 <ramon.pinho@vtal.com>
2023-02-04 03:09:15 +0000

    refactor: cleanup fetch session hook

--- src/hooks/useFetchSession/index.ts
+++ src/hooks/useFetchSession/index.ts
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { MOCK_OP_VTAL } from '../../context/auth/auth.mock';
 import config from '../../utils/config';
 import { IKeepAliveResponse } from '../../utils/interfaces';
 
@@ -13,28 +12,13 @@ const useFetchSession = () => {
     setLoading(true);
 
     fetch(KEEP_ALIVE_PROXY_URL, {
-      redirect: 'manual',
       credentials: 'same-origin',
     })
-      .then((res) => {
-        console.log('raw keep alive res: ', res);
-        if (!res.ok) {
-          console.log('not ok, redirecting...');
-
-          // window.location.href = res.url;
-          return res;
-        } else {
-          return res.json();
-        }
-      })
+      .then((res) => res.json())
       .then((data: IKeepAliveResponse) => {
-        console.log('formated res: ', data);
-
-        setUserInfo(() => MOCK_OP_VTAL);
+        setUserInfo(() => data);
       })
       .catch((error) => {
-        console.error('proxy error response: ', error);
-
         setError(() => error);
       })
       .finally(() => setLoading(() => false));
