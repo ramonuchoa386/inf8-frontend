5ea5c807713c1094b8339ad9670175602ea19115
vt415204 <ramon.pinho@vtal.com>
2023-02-04 01:33:56 +0000

    refactor: remove unnecessary config entries; remove commented out const

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,3 @@
-// const API_EXTERNAL_HOST = (): string => {
-//   switch (process.env.NODE_ENV) {
-//     case 'test':
-//       return 'https://novoportaloperacional-ti.vtal.com.br/';
-//     case 'production':
-//       return 'https://novoportaloperacional-ti.vtal.com.br/';
-//     default:
-//       return 'https://inf8-chinesewall-portaloperacional-hml.apps.ocpcorp.vtal.intra/api';
-//   }
-// };
-
 const config = {
   API_BASEURL:
     process.env.NODE_ENV === 'development'
@@ -17,10 +6,8 @@ const config = {
   FILEUPLOAD_ENDPOINT: '/file-upload',
   LOGS_ENDPOINT: '/logs',
   COMPANYIDS_ENDPOINT: '/listcompanyid',
-  MAX_FILE_SIZE: 5000000,
-  PROXY_URL: 'https://novoportaloperacional-ti.vtal.com.br',
-  API_PROXY_URL: '/api-inf8',
   KEEP_ALIVE_PROXY_URL: '/keep-alive',
+  MAX_FILE_SIZE: 5000000,
 };
 
 export default config;
