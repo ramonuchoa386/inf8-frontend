8f92ae1cc3a3ffd093621aeb6c2b8bf7e14f48f5
vt415204 <ramonuchoa386@gmail.com>
2023-02-06 19:13:56 -0300

    feat: comment out public page content

--- src/pages/public/login/index.tsx
+++ src/pages/public/login/index.tsx
@@ -1,42 +1,41 @@
-import React, { useContext, useState } from 'react';
-import AuthContext, { UserData } from '../../../context/auth';
-import {
-  MOCK_OP_VTAL,
-  MOCK_VW_VTAL,
-  MOCK_CP_MASTER,
-  MOCK_CP_ADMIN,
-  // MOCK_KEEP_ALIVE_RESPONSE,
-} from '../../../context/auth/auth.mock';
-import { Profiles } from '../../../utils/enums';
+import React from 'react';
+// import AuthContext, { UserData } from '../../../context/auth';
+// import {
+//   MOCK_OP_VTAL,
+//   MOCK_VW_VTAL,
+//   MOCK_CP_MASTER,
+//   MOCK_CP_ADMIN,
+//   // MOCK_KEEP_ALIVE_RESPONSE,
+// } from '../../../context/auth/auth.mock';
+// import { Profiles } from '../../../utils/enums';
 
 const Login = () => {
-  const { signIn } = useContext(AuthContext);
-  const [mockProfile, setMockProfile] =
-    useState<Omit<UserData, 'logged'>>(MOCK_OP_VTAL);
+  // const { signIn } = useContext(AuthContext);
+  // const [mockProfile, setMockProfile] = useState<Omit<UserData, 'logged'>>(MOCK_OP_VTAL);
 
-  const handleSelectProfile = (profile: Profiles) => {
-    switch (profile) {
-      case Profiles.VW_VTAL:
-        setMockProfile(() => MOCK_VW_VTAL);
-        break;
+  // const handleSelectProfile = (profile: Profiles) => {
+  //   switch (profile) {
+  //     case Profiles.VW_VTAL:
+  //       setMockProfile(() => MOCK_VW_VTAL);
+  //       break;
 
-      case Profiles.CP_MASTER:
-        setMockProfile(() => MOCK_CP_MASTER);
-        break;
+  //     case Profiles.CP_MASTER:
+  //       setMockProfile(() => MOCK_CP_MASTER);
+  //       break;
 
-      case Profiles.CP_ADMIN:
-        setMockProfile(() => MOCK_CP_ADMIN);
-        break;
+  //     case Profiles.CP_ADMIN:
+  //       setMockProfile(() => MOCK_CP_ADMIN);
+  //       break;
 
-      default:
-        setMockProfile(() => MOCK_OP_VTAL);
-        break;
-    }
-  };
+  //     default:
+  //       setMockProfile(() => MOCK_OP_VTAL);
+  //       break;
+  //   }
+  // };
 
   return (
     <>
-      <h1>Perfil</h1>
+      {/* <h1>Perfil</h1>
       <select onChange={(e) => handleSelectProfile(e.target.value as Profiles)}>
         <option value='OP_VTAL'>OP_VTAL</option>
         <option value='VW_VTAL'>VW_VTAL</option>
@@ -44,7 +43,7 @@ const Login = () => {
         <option value='CP_ADMIN'>CP_ADMIN</option>
       </select>
 
-      <button onClick={() => signIn(mockProfile)}>Login</button>
+      <button onClick={() => signIn(mockProfile)}>Login</button> */}
     </>
   );
 };
