954020c2ead679081430986bf640a6e3eaafd156
vt415204 <ramon.pinho@vtal.com>
2023-02-07 16:53:41 +0000

    feat: add API_CONTEXT variable to config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,7 @@ const config = {
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/api'
       : process.env.REACT_APP_API_BASEURL,
+  API_CONTEXT: '/api',
   FILEUPLOAD_ENDPOINT: '/file-upload',
   LOGS_ENDPOINT: '/logs',
   COMPANYIDS_ENDPOINT: '/listcompanyid',
