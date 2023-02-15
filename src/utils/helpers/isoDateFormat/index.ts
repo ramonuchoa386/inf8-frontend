a7ac7a72a136dccf943db5317937203e415e8ce7
vt415204 <ramon.pinho@vtal.com>
2023-02-15 20:10:35 +0000

    feat: remove date isoDate string replace

--- src/utils/helpers/isoDateFormat/index.ts
+++ src/utils/helpers/isoDateFormat/index.ts
@@ -6,7 +6,7 @@
  */
 
 export const ISODateFormat = (isoDate: string): string => {
-  const rawDate = Date.parse(isoDate.replace('Z', '-03:00'));
+  const rawDate = Date.parse(isoDate);
   const sendDate = new Date(rawDate);
 
   const date =
